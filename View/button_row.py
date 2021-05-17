75cd4d0c93d5f0f4c2c61f1f9780ced529751d07
losanov <losanov@ukr.net>
2021-05-18 01:49:16 +0300

    fix mistakes

--- View/button_row.py
+++ View/button_row.py
@@ -1,4 +1,5 @@
 class Button_row:
     def __init__(self):
         self.row = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '=', 'CHG']
-        self.cng_clicked = False
+        self.chg_clicked = False
+        self.is_equal = False
--- View/main_view.py
+++ View/main_view.py
@@ -6,24 +6,51 @@ import random
 from View.button_row import Button_row
 
 
-class Calculator(QtWidgets.QMainWindow, Ui_MainWindow, Button_row):
+class Calculator(QtWidgets.QMainWindow, Ui_MainWindow):
     widget_button_click_event = QtCore.pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.cng_clicked = Button_row().cng_clicked
+        self.cng_clicked = Button_row().chg_clicked
         self.row = Button_row().row
+        self.is_equal = Button_row().is_equal
         self.result_widget = Result()
         self.init_result_widget()
         self.widget_button_click_event.connect(self.button_click_processing)
         self.mult_buttons()
 
+    @staticmethod
+    def is_sign(value):
+        return value in ('+', '-', '*', '/', '=')
+
     def button_click_processing(self, value):
-        if value == 'CHG':
+        # first zero
+        if len(self.result_widget.get_result()) == 0 and Calculator.is_sign(value):
+            self.add_number('0')
+            self.add_number(value)
+        # non double signs
+        if len(self.result_widget.get_result()) > 0 and Calculator.is_sign(self.result_widget.get_result()[-1]):
+            if Calculator.is_sign(value):
+                value = ''
+                self.add_number(value)
+        # non def zero
+        if len(self.result_widget.get_result()) > 0 and self.result_widget.get_result()[-1] == '/':
+            if value == '0':
+                value = ''
+                self.add_number(value)
+        # clear result after equal
+        if self.is_equal and value.isdigit():
+            self.result_widget.set_result('')
+            self.is_equal = False
+        else:
+            self.is_equal = False
+
+        if value == 'CHG':    # change button flag
             self.cng_clicked = True
-        elif value == '=':
+        elif value == '=':   # equal
             self.result_widget.set_result(eval(self.get_result()))
+            self.is_equal = True
         else:
             self.add_number(value)
         self.mult_buttons()
